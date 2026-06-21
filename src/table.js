2984e5aec61cb58ecd7dd70d930e145729f619ae
keshavsoft <nknnkumar@yahoo.com>
2026-06-21 17:35:26 +0530

    p9

--- src/table.js
+++ src/table.js
@@ -1,8 +1,8 @@
 import { initShowTable }
-    from "../bin/table/v3/commands/table/template/v8/entry.js";
+    from "../bin/table/v3/commands/table/template/v9/entry.js";
 
 (async () => {
-    window.KSTableCompVersion = "v3.8";
+    window.KSTableCompVersion = "v3.9";
 
     window.KSTableComp = {};
 
